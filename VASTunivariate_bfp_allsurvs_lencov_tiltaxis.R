696c36cc75925dfc420760f207eb266805a5797c
Sarah Gaichas <sarah.gaichas@noaa.gov>
2022-02-12 13:31:13 -0500

    code for data meeting model runs

--- VASTunivariate_bfp_allsurvs_lencov_tiltaxis.R
+++ VASTunivariate_bfp_allsurvs_lencov_tiltaxis.R
@@ -153,7 +153,7 @@ settings = make_settings( n_x = 500,
 
 # select dataset and set directory for output
 
-season <- c("fall_500_allsurvs_no")
+season <- c("fall_500_allsurvs_lenno_tilt")
 
 working_dir <- here::here(sprintf("pyindex/allagg_%s/", season))
 
@@ -178,17 +178,90 @@ fit <- fit_model(
   b_i = as_units(bluepyagg_stn_fall[,'Catch_g'], 'g'),
   a_i = rep(1, nrow(bluepyagg_stn_fall)),
   v_i = bluepyagg_stn_fall$Vessel,
-  Q_ik = as.matrix(bluepyagg_stn_fall[,c("npiscsp")]),
+  Q_ik = as.matrix(bluepyagg_stn_fall[,c("meanpisclen", "npiscsp")]),
   #Use_REML = TRUE,
+  run_model = FALSE,
+  working_dir = paste0(working_dir, "/"))
+
+coastdistdat <-  readRDS(here("spatialdat/neus_coastdistdat.rds"))
+alongshore <- readRDS(here("spatialdat/neus.alongshore.axisdistdat.rds"))
+offshore <- readRDS(here("spatialdat/neus.offshore.axisdistdat.rds"))
+
+# extract northings and eastings 
+Z_gm = fit$spatial_list$loc_g 
+
+# convert northings/eastings to lat/lon
+tmpUTM = cbind('PID'=1,'POS'=1:nrow(Z_gm),'X'=Z_gm[,'E_km'],'Y'=Z_gm[,'N_km'])
+
+# create UTM object with correct attributes 
+attr(tmpUTM,"projection") = "UTM"
+attr(tmpUTM,"zone") = fit$extrapolation_list$zone - ifelse( fit$extrapolation_list$flip_around_dateline==TRUE, 30, 0 )
+
+# convert to lat/lon for merging with custom axis
+latlon_g = PBSmapping::convUL(tmpUTM)                                                         #$
+latlon_g = cbind( 'Lat'=latlon_g[,"Y"], 'Lon'=latlon_g[,"X"])
+latlon_g <- as.data.frame(latlon_g)
+
+# function to save the custom axis position that minimize Euclidean distance from each set of VAST coordinates
+get_length <- function(lon, lat, distdf) {
+  tmp <- distdf 
+  tmp$abs.diff.x2 = abs(distdf$x-lon)^2
+  tmp$abs.diff.y2 = abs(distdf$y-lat)^2
+  # get Euclidean dist from VAST points to all points along the coastal axis  
+  tmp$abs.diff.xy = sqrt(tmp$abs.diff.x2 + tmp$abs.diff.y2) 
+  tmp <- tmp[tmp$abs.diff.xy==min(tmp$abs.diff.xy),] # keep only the row with the minimum distance
+  return(tmp$lengthfromhere) # save the position of that row
+}
+
+# apply this to match each VAST knot with a position along the custom axis
+# use lat/lon to get coastal distance
+coast_km = NULL
+for(k in 1:nrow(latlon_g)){
+  out = get_length(lon=latlon_g$Lon[k], lat=latlon_g$Lat[k], distdf = coastdistdat)/1000 # works better in a for loop than in apply where it's hard to get the rowwise nature to work--revisit sometime?
+  coast_km <- c(coast_km, out)
+}
+
+along_km = NULL
+for(k in 1:nrow(latlon_g)){
+  out = get_length(lon=latlon_g$Lon[k], lat=latlon_g$Lat[k], distdf = alongshore)/1000 # works better in a for loop than in apply where it's hard to get the rowwise nature to work--revisit sometime?
+  along_km <- c(along_km, out)
+}
+
+offshore_km = NULL
+for(k in 1:nrow(latlon_g)){
+  out = get_length(lon=latlon_g$Lon[k], lat=latlon_g$Lat[k], distdf = offshore)/1000 # works better in a for loop than in apply where it's hard to get the rowwise nature to work--revisit sometime?
+  offshore_km <- c(offshore_km, out)
+}
+
+# bind the axis positions that match each knot back to Z_gm
+Z_gm = cbind( Z_gm, "coast_km"=coast_km, "along_km"=along_km, "offshore_km"=offshore_km )
+Z_gm_axes = colnames(Z_gm)
+
+#fit model with new coordinates
+fit <- fit_model(
+  settings = settings, 
+  Lat_i = bluepyagg_stn_fall$Lat, 
+  Lon_i = bluepyagg_stn_fall$Lon, 
+  t_i = bluepyagg_stn_fall$Year, 
+  b_i = as_units(bluepyagg_stn_fall[,'Catch_g'], 'g'),
+  a_i = rep(1, nrow(bluepyagg_stn_fall)),
+  v_i = bluepyagg_stn_fall$Vessel,
+  Q_ik = as.matrix(bluepyagg_stn_fall[,c("meanpisclen","npiscsp")]),
+  #Use_REML = TRUE,
+  Z_gm = Z_gm,
   working_dir = paste0(working_dir, "/"))
 
 # Plot results
 plot( fit,
       working_dir = paste0(working_dir, "/"))
 
+# Save fit
+saveRDS(fit, file = paste0(working_dir, "/fit.rds"))
+saveRDS(bluepyagg_stn_fall, file = paste0(working_dir, "/bluepyagg_stn_fall.rds"))
+
 # Run model spring
 
-season <- c("spring_500_allsurvs_no")
+season <- c("spring_500_allsurvs_lenno_tilt")
 
 working_dir <- here::here(sprintf("pyindex/allagg_%s/", season))
 
@@ -204,10 +277,82 @@ fit = fit_model( settings = settings,
                  b_i = as_units(bluepyagg_stn_spring[,'Catch_g'], 'g'), 
                  a_i = rep(1, nrow(bluepyagg_stn_spring)),
                  v_i = bluepyagg_stn_spring$Vessel,
-                 Q_ik = as.matrix(bluepyagg_stn_spring[,c("npiscsp")]),
+                 Q_ik = as.matrix(bluepyagg_stn_spring[,c("meanpisclen","npiscsp")]),
                 # Use_REML = TRUE,
+                 run_model = FALSE,
+                 working_dir = paste0(working_dir, "/"))
+
+coastdistdat <-  readRDS(here("spatialdat/neus_coastdistdat.rds"))
+alongshore <- readRDS(here("spatialdat/neus.alongshore.axisdistdat.rds"))
+offshore <- readRDS(here("spatialdat/neus.offshore.axisdistdat.rds"))
+
+# extract northings and eastings 
+Z_gm = fit$spatial_list$loc_g 
+
+# convert northings/eastings to lat/lon
+tmpUTM = cbind('PID'=1,'POS'=1:nrow(Z_gm),'X'=Z_gm[,'E_km'],'Y'=Z_gm[,'N_km'])
+
+# create UTM object with correct attributes 
+attr(tmpUTM,"projection") = "UTM"
+attr(tmpUTM,"zone") = fit$extrapolation_list$zone - ifelse( fit$extrapolation_list$flip_around_dateline==TRUE, 30, 0 )
+
+# convert to lat/lon for merging with custom axis
+latlon_g = PBSmapping::convUL(tmpUTM)                                                         #$
+latlon_g = cbind( 'Lat'=latlon_g[,"Y"], 'Lon'=latlon_g[,"X"])
+latlon_g <- as.data.frame(latlon_g)
+
+# function to save the custom axis position that minimize Euclidean distance from each set of VAST coordinates
+get_length <- function(lon, lat, distdf) {
+  tmp <- distdf 
+  tmp$abs.diff.x2 = abs(distdf$x-lon)^2
+  tmp$abs.diff.y2 = abs(distdf$y-lat)^2
+  # get Euclidean dist from VAST points to all points along the coastal axis  
+  tmp$abs.diff.xy = sqrt(tmp$abs.diff.x2 + tmp$abs.diff.y2) 
+  tmp <- tmp[tmp$abs.diff.xy==min(tmp$abs.diff.xy),] # keep only the row with the minimum distance
+  return(tmp$lengthfromhere) # save the position of that row
+}
+
+# apply this to match each VAST knot with a position along the custom axis
+# use lat/lon to get coastal distance
+coast_km = NULL
+for(k in 1:nrow(latlon_g)){
+  out = get_length(lon=latlon_g$Lon[k], lat=latlon_g$Lat[k], distdf = coastdistdat)/1000 # works better in a for loop than in apply where it's hard to get the rowwise nature to work--revisit sometime?
+  coast_km <- c(coast_km, out)
+}
+
+along_km = NULL
+for(k in 1:nrow(latlon_g)){
+  out = get_length(lon=latlon_g$Lon[k], lat=latlon_g$Lat[k], distdf = alongshore)/1000 # works better in a for loop than in apply where it's hard to get the rowwise nature to work--revisit sometime?
+  along_km <- c(along_km, out)
+}
+
+offshore_km = NULL
+for(k in 1:nrow(latlon_g)){
+  out = get_length(lon=latlon_g$Lon[k], lat=latlon_g$Lat[k], distdf = offshore)/1000 # works better in a for loop than in apply where it's hard to get the rowwise nature to work--revisit sometime?
+  offshore_km <- c(offshore_km, out)
+}
+
+# bind the axis positions that match each knot back to Z_gm
+Z_gm = cbind( Z_gm, "coast_km"=coast_km, "along_km"=along_km, "offshore_km"=offshore_km )
+Z_gm_axes = colnames(Z_gm)
+
+# fit model with new coordinates
+fit = fit_model( settings = settings, 
+                 Lat_i = bluepyagg_stn_spring[,'Lat'], 
+                 Lon_i = bluepyagg_stn_spring[,'Lon'], 
+                 t_i = bluepyagg_stn_spring[,'Year'], 
+                 b_i = as_units(bluepyagg_stn_spring[,'Catch_g'], 'g'), 
+                 a_i = rep(1, nrow(bluepyagg_stn_spring)),
+                 v_i = bluepyagg_stn_spring$Vessel,
+                 Q_ik = as.matrix(bluepyagg_stn_spring[,c("meanpisclen","npiscsp")]),
+                 # Use_REML = TRUE,
+                 Z_gm = Z_gm,
                  working_dir = paste0(working_dir, "/"))
 
 # Plot results
 plot( fit,
       working_dir = paste0(working_dir, "/")) 
+
+# Save fit
+saveRDS(fit, file = paste0(working_dir, "/fit.rds"))
+saveRDS(bluepyagg_stn_spring, file = paste0(working_dir, "/bluepyagg_stn_spring.rds"))
