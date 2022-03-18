3d98ddd9c3c6b5c08820bb00daf5c9a4139ea03d
Sarah Gaichas <sarah.gaichas@noaa.gov>
2022-03-18 14:59:53 -0400

    visualize spatial partition options

--- spatialdat/plotextgrid.R
+++ spatialdat/plotextgrid.R
@@ -55,10 +55,21 @@ MABGBgrid <- northwest_atlantic_grid %>%
 
 #preset EPUs dont go all the way inshore, neeed to redefine
 
-bfoffshore <- c(1010, 1730, 1690, 1650, 1050, 1090, 1230)
+bfoffshore <- c(1010, 1730, 1690, 1650, 1050, 1060, 1090, 1100, 1250, 1200, 1190, 1610)
 
 bfoffshoregrid <- northwest_atlantic_grid %>%
   filter(stratum_number %in% bfoffshore)
+
+#from mskeyrun vignette, EPU based on survey strata, replace built in VAST EPU
+#https://noaa-edab.github.io/ms-keyrun/articles/GBSurveySet.html
+
+MAB <- c(1010:1080, 1100:1120, 1600:1750, 3010:3450, 3470, 3500, 3510)
+GB  <- c(1090, 1130:1210, 1230, 1250, 3460, 3480, 3490, 3520:3550)
+GOM <- c(1220, 1240, 1260:1290, 1360:1400, 3560:3830)
+SS  <- c(1300:1352, 3840:3990)
+
+MABGBgrid <- northwest_atlantic_grid %>%
+  filter(stratum_number %in% c(MAB, GB))
 
 ggplot(data = ecodata::coast) +
   geom_sf() + 
@@ -67,5 +78,6 @@ ggplot(data = ecodata::coast) +
   geom_point(data = inshoregrid, aes(x = Lon, y = Lat), size=0.05, colour = "yellow") +
   geom_point(data = bfgrid, aes(x = Lon, y = Lat), size=0.05, colour = "blue") +
   geom_point(data = bfoffshoregrid, aes(x = Lon, y = Lat), size=0.05, colour = "blue", alpha=0.1) +
+  geom_point(data = MABGBgrid, aes(x = Lon, y = Lat), size=0.05, colour = "green", alpha=0.1) +
   coord_sf(xlim = c(-78, -65.5), ylim = c(35, 45))
   
