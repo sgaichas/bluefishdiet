8fc75b8d592029503c974b38eeb3fec5cb772b8b
Sarah Gaichas <sarah.gaichas@noaa.gov>
2022-04-13 12:24:31 -0400

    adds a plot function, sst in df looks ok

--- pull_OISST.R
+++ pull_OISST.R
@@ -100,6 +100,7 @@ raster_to_sstdf <- function(brick,
   return(sstdf)
 }
 
+# working with the rasters in memory, could read in files instead in name statement
 years <- 1985:2021
 for(i in years) {
   name <- get(paste0("test_",i))
@@ -118,5 +119,38 @@ library(FishStatsUtils)
 
 oneday <- sst2021[sst2021$month=="07" & sst2021$day=="04",] 
 
+jan15 <- sst2021[sst2021$month=="01" & sst2021$day=="15",] 
 
+mar15 <- sst2021[sst2021$month=="03" & sst2021$day=="15",]
 
+may15 <- sst2021[sst2021$month=="05" & sst2021$day=="15",]
+
+jul15 <- sst2021[sst2021$month=="07" & sst2021$day=="15",]
+
+sep15 <- sst2021[sst2021$month=="09" & sst2021$day=="15",]
+
+nov15 <- sst2021[sst2021$month=="11" & sst2021$day=="15",]
+
+dailysstplot <- function(oneday){
+  ggplot() +
+    geom_tile(data = oneday, aes(x = Lon, y = Lat, fill = sst)) +
+    geom_sf(data = ecodata::coast) +
+    geom_point(data = northwest_atlantic_grid, aes(x = Lon, y = Lat), size=0.05, alpha=0.1) +
+    scale_fill_gradientn(name = "Temp C",
+                         limits = c(0.5, 31),
+                         colours = c(scales::muted("blue"), "white",
+                                     scales::muted("red"), "black")) +
+    coord_sf(xlim = c(-77, -65), ylim = c(35, 45)) + 
+    ggtitle(paste("SST, mm dd yyyy:", unique(oneday$month),
+                   unique(oneday$day), unique(oneday$year), sep = " "))
+}
+
+dailysstplot(oneday)
+
+par(mfrow=c(2,3))
+dailysstplot(jan15)
+dailysstplot(mar15)
+dailysstplot(may15)
+dailysstplot(jul15)
+dailysstplot(sep15)
+dailysstplot(nov15)
